03aedf4b2c35c57e3320387747c71383b156c6e2
Sukhov Alexander <sukhov2003sasha@mail.ru>
2025-04-14 22:49:57 +0300

    Изменения в обучении

--- src/model.py
+++ src/model.py
@@ -7,12 +7,14 @@ import os
 class DQN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(10, 512)
-        self.fc2 = nn.Linear(512, 6)
+        self.fc1 = nn.Linear(12, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 6)
     
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
     
     def save(self, file_name):
@@ -114,4 +116,4 @@ class Q_table_trainer():
         if not done:
             Q_table[state_transformed][action] += self.lr*(reward + self.gamma*np.max(Q_table[next_state_transformed]) - Q_table[state_transformed][action])
         else:
-            Q_table[state_transformed][action] += self.lr*(reward - Q_table[state_transformed][action])
+            Q_table[state_transformed][action] = reward
