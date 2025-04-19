c70aaa6baeaaf534bf0d934abe704dfc1e54326d
Sukhov Alexander <sukhov2003sasha@mail.ru>
2025-04-19 21:18:09 +0300

    longEasyDQN

--- src/model.py
+++ src/model.py
@@ -8,13 +8,13 @@ class DQN(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(13, 169)
-        self.fc2 = nn.Linear(169, 169)
+        # self.fc2 = nn.Linear(169, 169)
         self.fc3 = nn.Linear(169, 6)
 
     
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
     
